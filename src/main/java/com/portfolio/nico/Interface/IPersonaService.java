4b3f47b991b08c906653298f7eb2ae809f18dab1
Nico <nicotapia02@outlook.com>
2022-12-30 17:42:12 -0300

    primer commit

---
+++ src/main/java/com/portfolio/nico/Interface/IPersonaService.java
@@ -0,0 +1,19 @@
+package com.portfolio.nico.Interface;
+
+import com.portfolio.nico.Entity.Persona;
+import java.util.List;
+
+
+public interface IPersonaService {
+    //Traer una lista de personas
+    public List<Persona> getPersona();
+    
+    //Guardar un objeto de tipo persona
+    public void savePersona(Persona persona);
+    
+    //Eliminar un objeto buscado por ID
+    public void deletePersona(Long id);
+    
+    //Buscar una persona por ID
+    public Persona findPersona(Long id);
+}
